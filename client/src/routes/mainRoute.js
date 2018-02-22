b46125dc0b5f231c3ef59621d752c7148e7be591
Unknown <hoang_giang_bk@yahoo.com>
2018-02-22 22:14:39 +0700

    WIP

--- client/src/routes/mainRoute.js
+++ client/src/routes/mainRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import {Dashboard, LoginOrRegister, About} from '../pages';
-import RootContainer from '../container/RootContainer';
 
 export default () => {
   return (
